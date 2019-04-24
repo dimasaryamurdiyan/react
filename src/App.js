b01fc7545cc18c9d2d4d5ed53c371b0aaa87c75a
DESKTOP-78512H6\Dimas Arya M <dimasaryamurdiyan123@gmail.com>
2019-04-24 16:35:04 +0700

    try grid on react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 import Product from './Product';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Product judul="Charger HP" harga="Rp. 10.000"/>
-          <Product judul="Charger HP" harga="Rp. 10.000"/>
-          <Product judul="Charger Laptop" harga="Rp. 100.000"/>
-          <Product judul="Charger Motor" harga="Rp. 1.000.000"/>
-        </header>
+      <div className="row">
+        <div className="col m4">
+          
+          <div className="card">
+            <div className="card-content">
+                <div className="row">
+                <h3 className="center blue-grey-text text-lighten-1">TAMBAH PRODUK</h3>
+                    <form className="col s12">
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="judul" type="text" className="validate"/>
+                                <label for="judul">Judul</label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input id="keterangan" type="text" className="validate"/>
+                                <label for="keterangan">Keterangan</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <a class="waves-effect waves-light btn-small">Tambah</a>
+            </div>
+          </div>
+        </div>
+        <div className="col m8">
+            <Product judul="Charger HP" keterangan="Lorem opsum sit dolor amet"/>
+            <Product judul="Charger HP" keterangan="Lorem opsum sit dolor amet"/>
+            <Product judul="Charger Laptop" keterangan="Lorem opsum sit dolor amet"/>
+            <Product judul="Charger Motor" keterangan="Lorem opsum sit dolor amet"/>
+        </div>
       </div>
     );
   }
--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import './Product.css';
+// import './Product.css';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 class Product extends React.Component {
     render() {
         return (
-            <div className="box">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h3 className="ungu">{this.props.judul}</h3>
-                <h5 className="orange">{this.props.harga}</h5>
-                <button className="btn btn-blue">Tambah ke Keranjang</button>
+            <div className="col m4">
+              <div className="card">
+                <div className="card-image">
+                    <img src={logo}/>
+                    <br/><br/>
+                    <span className="card-title blue-grey-text text-lighten-1">{this.props.judul}</span>
+                    <a className="btn-floating halfway-fab waves-effect waves-light red">
+                     <i className="material-icons">add</i>
+                    </a>
+                </div>
+                <div className="card-content">
+                  <p>{this.props.keterangan}</p>
+                </div>
+                <div className="card-action">
+                <a class="waves-effect waves-light red darken-4 btn-small"><i class="material-icons right">restore_from_trash</i>button</a>
+                </div>
+              </div>
             </div>
+          
+            // <div className="box">
+            //     <img src={logo} className="App-logo" alt="logo"/>
+            //     <h3 className="ungu">{this.props.judul}</h3>
+            //     <h5 className="orange">{this.props.harga}</h5>
+            //     <button className="btn btn-blue">Tambah ke Keranjang</button>
+            // </div>
         );
     }
 }
