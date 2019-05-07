71dd6e797fa32cdcfc227c53aa392d00f1e22d53
DESKTOP-78512H6\Dimas Arya M <dimasaryamurdiyan123@gmail.com>
2019-05-07 08:35:26 +0700

    first spa

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
 import React, { Component } from 'react';
 import Navbar from './MyAppBar.js';
-import Card from './FoodCard.js';
 import Map from './SimpleMap';
+import { Link } from 'react-router-dom';
+import Main from './components/page/Main.js';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      foods : [ 
-        {
-          title : 'Fried rice with onion',
-          date : '20 April 2019',
-          description : 'Lorem ipsum sit dolor amet'
-        },
-        {
-          title : 'Mendoan',
-          date : '21 April 2019',
-          description : 'Lorem ipsum sit dolor amet'
-        },
-      ]
-    }
-  }
-
   render() {
     return (
-    <div>
-     <Navbar/>
-     <br/>
-     <Map/>
-     {this.state.foods.map((data)=>{
-       return(
-         <div>
-           <Card title={data.title} date={data.date} description={data.description}/>
-           <br/>
-         </div>
-       )
-     })}
-     
-     </div>
+      <div>
+        <Link to='/home'> Home </Link>&nbsp;&nbsp;
+        <Link to='/profile'> Profile </Link>&nbsp;&nbsp;
+        <Link to='/portofolio'> Portofolio </Link>&nbsp;&nbsp;
+        <Link to='/contact'> Contact </Link>&nbsp;&nbsp;
+        <hr/>
+        <Main/>
+      </div>
     );
   }
 }
